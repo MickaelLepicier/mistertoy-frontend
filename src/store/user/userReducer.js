a3943a5cf187c07d82876abbf03685493a684475
MickaelLepicier <mickael233@gmail.com>
2025-04-17 12:02:47 +0300

    Add userReducer and userAction

--- src/store/user/userReducer.js
+++ src/store/user/userReducer.js
@@ -1,13 +1,22 @@
-import { userService } from "../../services/user.service"
-
+import { userService } from '../../services/user.service'
 
 export const SET_USER = 'SET_USER'
-// export const SET_USER = ''
+// export const SET_USER_SCORE = 'SET_USER_SCORE'
 
 const initialState = {
-    user: userService.getLoggedinUser()
+  user: userService.getLoggedinUser()
 }
 
-export function userReducer(state = initialState, cmd ={} ){
-    //
+export function userReducer(state = initialState, cmd = {}) {
+  switch (cmd.type) {
+    case SET_USER:
+      return { ...state, user: cmd.user }
+
+    // case SET_USER_SCORE:
+    //   const loggedInUser = { ...state.loggedInUser, score: action.score }
+    //   return { ...state, loggedInUser }
+
+    default:
+      return state
+  }
 }
