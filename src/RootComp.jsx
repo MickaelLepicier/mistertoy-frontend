a46755f17ab3213d3bfe20f128f5737a61c24d99
MickaelLepicier <mickael233@gmail.com>
2025-04-23 16:01:33 +0300

    Add basic css

--- src/RootComp.jsx
+++ src/RootComp.jsx
@@ -1,43 +1,36 @@
-import { Provider } from "react-redux"
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
-import { store } from "./store/store"
-import { AppHeader } from "./cmps/AppHeader"
-import { AppFooter } from "./cmps/AppFooter"
-import { UserMsg } from "./cmps/UserMsg"
-
+import { Provider } from 'react-redux'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { store } from './store/store'
+import { AppHeader } from './cmps/AppHeader'
+import { AppFooter } from './cmps/AppFooter'
+import { UserMsg } from './cmps/UserMsg'
 
 // imports:
 // HomePage
 
 // ToyIndex (Filter, ToyList, ToyPreview)
 // ToyDetails
-// 
+//
 
 // TODO keep going ;)
 
- function App(){
-
-
-    return(
-        <Provider store={store}>
-            <Router>
-
-            <section>
-                <AppHeader/>
-                <main>
-                    <Routes>
-                        <Route path='/' element={<HomePage/>}/>
-
-                    </Routes>
-                </main>
-                <AppFooter/>
-            </section>
-
-            </Router>
-            <UserMsg/>
-        </Provider>
-    )
-
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <section>
+          <AppHeader />
+          <main>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+            </Routes>
+          </main>
+          <AppFooter />
+        </section>
+      </Router>
+      <UserMsg />
+    </Provider>
+  )
 }
 
 export default App
