52f3bcbc351590b7b87f1641137f7b44144e976e
MickaelLepicier <mickael233@gmail.com>
2025-05-07 22:03:01 +0300

    fix bug in vit.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,48 @@
-import path from 'path'
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-        outDir: '../mistertoy-backend/public',
-        emptyOutDir: true,
+    outDir: '../mistertoy-backend/public',
+    emptyOutDir: true,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/style/setup/vars.scss";`,
+      },
     },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: `@import "${path.resolve(__dirname, 'src/assets/style/setup/vars.scss')}";`
-        }
-      }
-    }
-
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
+
+
+
+
+// import path from 'path'
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//         outDir: '../mistertoy-backend/public',
+//         emptyOutDir: true,
+//     },
+//     css: {
+//       preprocessorOptions: {
+//         scss: {
+//           additionalData: `@import "${path.resolve(__dirname, 'src/assets/style/setup/vars.scss')}";`
+//         }
+//       }
+//     }
+
+// })
